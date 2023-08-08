1ac1b3c82585cc69332f781479e19c188d7a4056
Gregg Tavares <github@greggman.com>
2023-08-07 22:02:35 -0700

    added timing
    On M1, slow is ~100x slower than fast
    
    (for 1024x1024 all black texture, so most contention)
    slow is generally 435 million nanoseconds (435ms)
    fast is generally 6 million nanoseconds (6ms)

---
+++ webgpu/resources/js/timing-helper.js
@@ -0,0 +1,90 @@
+function assert(cond, msg = '') {
+  if (!cond) {
+    throw new Error(msg);
+  }
+}
+
+export default class TimingHelper {
+  #device;
+  #canTimestamp;
+  #querySets;
+  #currentSet;
+  #state = 'free';
+
+  constructor(device) {
+    this.#device = device;
+    this.#canTimestamp = device.features.has('timestamp-query');
+    this.#querySets = [];
+    this.#currentSet = undefined;
+  }
+
+  #getQuerySet() {
+    const device = this.#device;
+    if (this.#querySets.length === 0) {
+      const querySet = device.createQuerySet({
+         type: 'timestamp',
+         count: 2,
+      });
+      const resolveBuffer = device.createBuffer({
+        size: 2 * 8,
+        usage: GPUBufferUsage.QUERY_RESOLVE | GPUBufferUsage.COPY_SRC,
+      });
+      const resultBuffer = device.createBuffer({
+        size: 2 * 8,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
+      });
+      this.#querySets.push({querySet, resolveBuffer, resultBuffer});
+    }
+    return this.#querySets.pop();
+  }
+  addTimestampToPass(descriptor = {}) {
+    if (this.#canTimestamp) {
+      assert(this.#state === 'free', 'state not free');
+      this.#state = 'need resolve';
+
+      assert(!this.#currentSet);
+      this.#currentSet = this.#getQuerySet();
+
+      descriptor.timestampWrites = {
+        querySet: this.#currentSet.querySet,
+        beginningOfPassWriteIndex: 0,
+        endOfPassWriteIndex: 1,
+      };
+    }
+    return descriptor;
+  }
+
+  resolveTiming(encoder) {
+    if (!this.#canTimestamp) {
+      return;
+    }
+    assert(!!this.#currentSet);
+    assert(this.#state === 'need resolve', 'must call addTimestampToPass');
+    this.#state = 'wait for result';
+
+    const { querySet, resolveBuffer, resultBuffer } = this.#currentSet;
+
+    encoder.resolveQuerySet(querySet, 0, 2, resolveBuffer, 0);
+    encoder.copyBufferToBuffer(resolveBuffer, 0, resultBuffer, 0, resultBuffer.size);
+  }
+
+  async getResult() {
+    if (!this.#canTimestamp) {
+      return 0;
+    }
+    assert(!!this.#currentSet);
+    assert(this.#state === 'wait for result', 'must call resolveTiming');
+    this.#state = 'free';
+
+    const q = this.#currentSet;
+    this.#currentSet = undefined;
+
+    const { resultBuffer } = q;
+    await resultBuffer.mapAsync(GPUMapMode.READ);
+    const times = new BigInt64Array(resultBuffer.getMappedRange());
+    const duration = Number(times[1] - times[0]);
+    resultBuffer.unmap();
+    this.#querySets.push(q);
+    return duration;
+  }
+}
